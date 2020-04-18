68b5bf40c1446858575938b510153b5395bf0983
nickhoke24 <51420208+nickhoke24@users.noreply.github.com>
2020-04-18 13:49:36 -0400

    Separated array items with comma, added semicolon to end of array

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 var pokemonList = [
-    { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']}
-    { name: 'Charmander', height: 0.6, types: 'fire'}
+    { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] },
+    { name: 'Charmander', height: 0.6, types: 'fire'},
     { name: 'Squirtle', height: 0.5, types: 'water'} 
-]
+];
