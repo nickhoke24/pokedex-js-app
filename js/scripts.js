5610f68495a23545eb4497d781c159270510a786
nickhoke24 <51420208+nickhoke24@users.noreply.github.com>
2020-04-19 19:35:54 -0400

    Organized Pokemon array, changed Charmander to Charizard

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
 var pokemonList = [
-    { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] },
-    { name: 'Charmander', height: 0.6, types: 'fire'},
-    { name: 'Squirtle', height: 0.5, types: 'water'} 
+    { 
+        name: 'Bulbasaur', 
+        height: 0.7, types: 
+        ['grass', 'poison'] 
+    },
+    { 
+        name: 'Charizard', 
+        height: 1.7, 
+        types: 'fire'
+    },
+    { 
+        name: 'Squirtle', 
+        height: 0.5, 
+        types: 'water'
+    } 
 ];
