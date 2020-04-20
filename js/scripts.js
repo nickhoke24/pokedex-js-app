219f2283c8490ebc32a0072f7c684ea3fe4d2a85
nickhoke24 <51420208+nickhoke24@users.noreply.github.com>
2020-04-20 18:07:37 -0400

    Formatted scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,26 @@
-var pokemonList = [
-    { 
-        name: 'Bulbasaur', 
-        height: 0.7, types: 
-        ['grass', 'poison'] 
+var pokemonList = [{
+        name: "Bulbasaur",
+        height: 0.7,
+        types: ["grass", "poison"],
     },
-    { 
-        name: 'Charizard', 
-        height: 1.7, 
-        types: 'fire'
+    {
+        name: "Charizard",
+        height: 1.7,
+        types: "fire",
     },
-    { 
-        name: 'Squirtle', 
-        height: 0.5, 
-        types: 'water'
-    } 
+    {
+        name: "Squirtle",
+        height: 0.5,
+        types: "water",
+    },
 ];
 
 for (var i = 0; i < pokemonList.length; i++) {
-    document.write(pokemonList[i].name + ('Height' + pokemonList[i].height))
-if (pokemonList[i].height > 1.0) {
-    document.write("Wow, that's big!");
+    document.write(
+        pokemonList[i].name + (" (Height " + pokemonList[i].height) + ")"
+    );
+    if (pokemonList[i].height > 1.0) {
+        document.write(" - Wow, that's big!");
     }
+    document.write("<br>");
 }
-    
