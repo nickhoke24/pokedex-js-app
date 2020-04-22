77603052c4d6d70d59b988aaffbb73a474a0c162
nickhoke24 <51420208+nickhoke24@users.noreply.github.com>
2020-04-22 16:04:43 -0400

    Commented out [for] loop and made it a [forEach] loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,35 @@
-var pokemonList = [{
-        name: "Bulbasaur",
-        height: 0.7,
-        types: ["grass", "poison"],
-    },
-    {
-        name: "Charizard",
-        height: 1.7,
-        types: "fire",
-    },
-    {
-        name: "Squirtle",
-        height: 0.5,
-        types: "water",
-    },
+var pokemonList = [
+  {
+    name: "Bulbasaur",
+    height: 0.7,
+    types: ["grass", "poison"],
+  },
+  {
+    name: "Charizard",
+    height: 1.7,
+    types: "fire",
+  },
+  {
+    name: "Squirtle",
+    height: 0.5,
+    types: "water",
+  },
 ];
 
-for (var i = 0; i < pokemonList.length; i++) {
-    document.write(
-        pokemonList[i].name + (" (Height " + pokemonList[i].height) + ")"
-    );
-    if (pokemonList[i].height > 1.0) {
-        document.write(" - Wow, that's big!");
-    }
-    document.write("<br>");
-}
+// for (var i = 0; i < pokemonList.length; i++) {
+//   document.write(
+//     pokemonList[i].name + (" (Height " + pokemonList[i].height) + ")"
+//   );
+//   if (pokemonList[i].height > 1.0) {
+//     document.write(" - Wow, that's big!");
+//   }
+//   document.write("<br>");
+// }
+
+pokemonList.forEach(function (pokemon) {
+  document.write(pokemon.name + " (Height " + pokemon.height + ")");
+  if (pokemon.height > 1.0) {
+    document.write(" - Wow, that's big!");
+  }
+  document.write("<br>");
+});
