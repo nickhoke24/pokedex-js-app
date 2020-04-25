383ba57ce4207f6cf4fc085e05ca0f4313300a98
nickhoke24 <51420208+nickhoke24@users.noreply.github.com>
2020-04-25 14:19:04 -0400

    Wrapped pokemonList array in IIFE, created pokemonRepositry var

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,24 @@
-var pokemonList = [
-  {
-    name: "Bulbasaur",
-    height: 0.7,
-    types: ["grass", "poison"],
-  },
-  {
-    name: "Charizard",
-    height: 1.7,
-    types: "fire",
-  },
-  {
-    name: "Squirtle",
-    height: 0.5,
-    types: "water",
-  },
-];
+var pokemonRepository = (function () {
+  var pokemonList = [
+    {
+      name: "Bulbasaur",
+      height: 0.7,
+      types: ["grass", "poison"],
+    },
+    {
+      name: "Charizard",
+      height: 1.7,
+      types: "fire",
+    },
+    {
+      name: "Squirtle",
+      height: 0.5,
+      types: "water",
+    },
+  ];
+})();
 
-pokemonList.forEach(function (pokemon) {
+pokemonRepository.forEach(function (pokemon) {
   document.write(pokemon.name + " (Height " + pokemon.height + ")");
   if (pokemon.height > 1.0) {
     document.write(" - Wow, that's big!");
