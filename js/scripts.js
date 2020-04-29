eee36973fada1644fbcdf1a34ff22d6de9cbeda1
nickhoke24 <51420208+nickhoke24@users.noreply.github.com>
2020-04-29 18:12:33 -0400

    Added showDetails Function, removed comment

--- js/scripts.js
+++ js/scripts.js
@@ -32,9 +32,28 @@ var pokemonRepository = (function () {
     return pokemonList;
   }
 
+  function addListItem(pokemon = {}) {
+    var pokedexList = document.querySelector(".pokemon-list");
+    var listitem = document.createElement("li");
+    var button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("pokemon-button");
+    listitem.appendChild(button);
+    pokedexList.appendChild(listitem);
+    button.addEventListener("click", function (event) {
+      showDetails(pokemon);
+    });
+  }
+
+  function showDetails(pokemon) {
+    console.log(pokemon);
+    document.write(pokemon.name);
+  }
+
   return {
     add: add,
     getAll: getAll,
+    addListItem: addListItem,
   };
 })();
 
@@ -43,18 +62,5 @@ pokemonRepository.add({ name: "Pikachu", height: 0.3, types: ["electric"] });
 console.log(pokemonRepository.getAll());
 
 pokemonRepository.getAll().forEach(function (pokemon) {
-  var pokedexList = document.querySelector(".pokemon-list");
-  var listitem = document.createElement("li");
-  var button = document.createElement("button");
-  button.innerText = pokemon.name;
-  button.classList.add("pokemon-button");
-  listitem.appendChild(button);
-  pokedexList.appendChild(listitem);
+  pokemonRepository.addListItem(pokemon);
 });
-
-//   document.write(pokemon.name + " (Height " + pokemon.height + ")");
-//   if (pokemon.height > 1.0) {
-//     document.write(" - Wow, that's big!");
-//   }
-//   document.write("<br>");
-// });
