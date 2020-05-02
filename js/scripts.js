00b8210814f2c83f1d24bbc728997058ec82c650
nickhoke24 <51420208+nickhoke24@users.noreply.github.com>
2020-05-01 20:14:33 -0400

    Added function loadDetails(item)

--- js/scripts.js
+++ js/scripts.js
@@ -66,6 +66,20 @@ var pokemonRepository = (function () {
     })
   }
 
+  function loadDetails(item) {
+    var url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      // Now we add the details to the item
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
+
   return {
     add: add,
     getAll: getAll,
